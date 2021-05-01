35cdb84bf0558877bdd62bd47c809428715c1b66
herronk2 <kyle.herron2@mail.dcu.ie>
2021-05-01 01:14:51 +0100

    Henlo

--- bot.py
+++ bot.py
@@ -2,17 +2,14 @@ import os
 import discord
 from dotenv import load_dotenv
 
-#  a6256877-c8a2-4946-943a-ede987488d68
-#  valorant.gg token
-
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER = os.getenv('DISCORD_SERVER')
 
 
-
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
@@ -28,4 +25,3 @@ async def on_ready():
     print(f'Server Members:\n - {members}')
 
 client.run(TOKEN)
-
